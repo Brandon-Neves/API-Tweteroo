dd9f5050261aa1ab59d8329e25705312bb32b749
Brandon-Neves <84950033+Brandon-Neves@users.noreply.github.com>
2023-04-07 19:19:24 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const users = []
+const tweets = []
+let newUsers
+
+app.post('/sign-up', (req, res) => {
+  newUsers = req.body
+  users.push(newUsers)
+  res.send('OK')
+})
+
+app.post('/tweets', (req, res) => {
+  //const user = users.find(i => i.username === newUsers)
+  if (newUsers.username !== req.body.username) {
+    res.send('UNAUTHORIZED')
+    return
+  }
+  const newTweets = req.body
+  const user = {
+    username: newUsers.username,
+    avatar: newUsers.avatar,
+    tweet: newTweets.tweet
+  }
+  tweets.push(user)
+  res.send('OK')
+})
+
+app.get('/tweets', (req, res) => {
+  if (!tweets) {
+    res.send([])
+    return
+  }
+  res.send(tweets.slice(-10).reverse())
+})
+
+app.get('/sign-up', (req, res) => {
+  res.send(users)
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log('Servidor rodando'))
